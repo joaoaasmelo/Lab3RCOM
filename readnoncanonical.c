ff324790c61b006f7ea21a6b55120070d4a55919
joaoaasmelo <joaomelo387@gmail.com>
2024-04-24 17:20:33 +0100

    24-04 17:20

--- readnoncanonical.c
+++ readnoncanonical.c
@@ -219,6 +219,99 @@ void data_transfer_sender(int *fd, int fl){
 
 }
 
+void termination_receive(int *fd, int fl) {
+    const char FLAG = 0x5c;
+    const char A = 0x03;
+    char C;
+    switch(fl){
+        case 0:
+            C = 0x0a;
+            break;
+        case 1:
+            C = 0x06;
+            break;
+    }
+    const char BCC1 = A ^ C;
+    
+    state_t maqstate = START;
+    char buf[2]; // Tamanho do array buf ajustado para 2, se der erro mudar para 256
+
+    while (maqstate != STOP) {
+        int res = read(*fd, buf, 1);
+        buf[res] = '\0';
+
+        switch (maqstate) {
+               case START:
+                if (buf[0] == FLAG) {
+                    maqstate = FLAG_RCV;
+                    printf("FLAG = %x\n", buf[0]);
+                }
+                break;
+            case FLAG_RCV:
+                if (buf[0] == A) {
+                    maqstate = A_RCV;
+                    printf("A = %x\n", buf[0]);
+                } else if (buf[0] == FLAG) {
+                    maqstate = FLAG_RCV;
+                } else {
+                    maqstate = START;
+                }
+                break;
+            case A_RCV:
+                if (buf[0] == C) {
+                    maqstate = C_RCV;
+                    printf("C = %x\n", buf[0]);
+                } else if (buf[0] == FLAG) {
+                    maqstate = FLAG_RCV;
+                } else {
+                    maqstate = START;
+                }
+                break;
+            case C_RCV:
+                if (buf[0] == BCC1) {
+                    maqstate = BCC_OK;
+                    printf("BCC1 = %x\n", buf[0]);
+                } else if (buf[0] == FLAG) {
+                    maqstate = FLAG_RCV;
+                } else {
+                    maqstate = START;
+                }
+                break;
+            case BCC_OK:
+                if (buf[0] == FLAG) {
+                    maqstate = STOP;
+                    printf("FLAG = %x\n", buf[0]);
+                } else {
+                    maqstate = START;
+                }
+                break;
+        }
+    }
+    
+    printf("--------------------------------\n");
+    return; // Termina a função
+}
+
+void termination_sender(int *fd) {
+    const char FLAG = 0x5c;
+    const char A = 0x01;
+    char C = 0x0a;
+    const char BCC1 = A ^ C;
+    
+    char buf[5];
+
+    buf[0] = FLAG;
+    buf[1] = A;
+    buf[2] = C;
+    buf[3] = BCC1;
+    buf[4] = FLAG;
+
+    int res = write(*fd, buf, 5);
+    printf("%d bytes written\n", res);
+    
+    return; // Termina a função
+}
+
 int main(int argc, char** argv)
 {
     int fd, c, res;
@@ -269,6 +362,8 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
+
+    //establishment
     
     establishment_receive(&fd);
 
@@ -289,6 +384,14 @@ int main(int argc, char** argv)
     data_transfer_receive(&fd, 1);
 
     data_transfer_sender(&fd, 1);
+
+    //termination
+
+    termination_receive(&fd, 0);
+
+    termination_sender(&fd);
+
+    termination_receive(&fd, 1);
 
     sleep(1);
 
